fc4abf44636ba26a958a79838682cb00c4623d55
Karine <karine.pistili@gmail.com>
2020-12-09 14:20:30 -0300

    init with empty project

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,10 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:carousel_slider/carousel_slider.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -32,96 +27,16 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  File _image;
-  final picker = ImagePicker();
-  List<File> _carouselImages = [];
-
-  Future _addImage() async {
-    final pickedFile = await picker.getImage(source: ImageSource.camera);
-
-    setState(() {
-      if (pickedFile != null) {
-        _image = File(pickedFile.path);
-        _carouselImages.add(_image);
-      } else {
-        print('No image selected.');
-      }
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    final List<Widget> imageSliders =
-        _carouselImages.map((file) => ImageCard(file)).toList();
-
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
       body: Center(
-        child: Container(
-            padding: EdgeInsets.only(top: 20),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                CarouselSlider(
-                  options: CarouselOptions(
-                    autoPlay: true,
-                    aspectRatio: 2.0,
-                    enlargeCenterPage: true,
-                  ),
-                  items: imageSliders,
-                ),
-                RaisedButton.icon(
-                  onPressed: () {
-                     _addImage();
-                  },
-                  color: Colors.blue,
-                  icon: Icon(Icons.add_a_photo, color: Colors.white,),
-                  label: Text('New photo', style: TextStyle(color: Colors.white),),
-                )
-              ],
-            )),
-      ),
-    );
-  }
-}
-
-class ImageCard extends StatelessWidget {
-  final fileImage;
-
-  ImageCard(this.fileImage);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(5.0),
-      child: ClipRRect(
-          borderRadius: BorderRadius.all(Radius.circular(5.0)),
-          child: Stack(
-            children: <Widget>[
-              Image.file(fileImage, fit: BoxFit.cover, width: 1000.0),
-              Positioned(
-                bottom: 0.0,
-                left: 0.0,
-                right: 0.0,
-                child: Container(
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: [
-                        Color.fromARGB(200, 0, 0, 0),
-                        Color.fromARGB(0, 0, 0, 0)
-                      ],
-                      begin: Alignment.bottomCenter,
-                      end: Alignment.topCenter,
-                    ),
-                  ),
-                  padding:
-                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                ),
-              ),
-            ],
-          )),
+        child: Text('hello world'),
+      )
     );
   }
 }
